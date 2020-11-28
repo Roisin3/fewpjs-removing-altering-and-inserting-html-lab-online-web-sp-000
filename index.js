2e2b397bd2621559d5b96a28937bad7505a42b8f
Michael Lynch <michael.lynch8211@gmail.com>
2020-11-28 06:03:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code here!
 main.remove()
-let newHeader = document.querySelector("h1#victory");
+newHeader = document.createElement("h1#victory");
 newHeader.innerHTML = 'YOUR-NAME is the champion';
 
